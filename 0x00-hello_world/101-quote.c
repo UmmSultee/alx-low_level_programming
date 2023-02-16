8500e809b3334f83f06e7428e83e52b6705d7db5
UmmSultee <harounummie@gmail.com>
2023-02-16 10:22:32 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,24 @@
 #!/bin/bash
-#include <stdio.h>
-#include <unistd.h>
-/**
-* main -A c program that prints a
-line to the standard error
-* Return:1(success)
-*/
-int main(void)
-{
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return(1);
-}
+#include <stdio.h>
+
+ /**
+  * main - prints to string
+  * Description: Prints "and that piece of art is useful.." without puts
+  * Return: 1
+  */
+
+ int main(void)
+ {
+ char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ long l = 59;
+ long fd = 1;
+ long syscall = 1;
+ long ret = 0;
+ __asm__ ("syscall"
+ : "=a" (ret)
+ : "a" (syscall),
+ "D" (fd),
+ "S" (s),
+ "d" (l));
+ return (1);
+ }
