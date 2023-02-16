2442062c0e90a83a4dbbe3cf12fd3c68ca59a7c0
UmmSultee <harounummie@gmail.com>
2023-02-16 10:18:41 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #!/bin/bash
-#include <stdio.h>
-/**
-* main -A program that print a line
-using the printf function*  Return: 0
-(success)
-*/
+#include <stdio.h> 
+  
+ /** 
+  * main - Print a string 
+  * Description: use printf 
+  * Return: 0 
+  */ 
+  
+ int main(void) 
+ { 
+         printf("%s", "with proper grammar, but the outcome is a piece of art,\n"); 
+         return (0); 
+ }
